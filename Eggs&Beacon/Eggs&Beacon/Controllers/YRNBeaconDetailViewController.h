968dda5aa4fabd0a18d09cfb6605762a5a8a4616
Mouhcine <mouhcine.elamine@icloud.com>
2013-12-29 17:29:13 +0100

    Changed implementation to show information of a specific CLBeacon object

--- Eggs&Beacon/Eggs&Beacon/Controllers/YRNBeaconDetailViewController.h
+++ Eggs&Beacon/Eggs&Beacon/Controllers/YRNBeaconDetailViewController.h
@@ -7,7 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 
 @interface YRNBeaconDetailViewController : UIViewController
+
+@property (nonatomic, strong) CLBeacon *beacon;
 
 @end
--- Eggs&Beacon/Eggs&Beacon/Controllers/YRNBeaconDetailViewController.m
+++ Eggs&Beacon/Eggs&Beacon/Controllers/YRNBeaconDetailViewController.m
@@ -8,9 +8,7 @@
 
 #import "YRNBeaconDetailViewController.h"
 
-#import "YRNBeaconManager.h"
-
-@interface YRNBeaconDetailViewController () <YRNBeaconManagerDelegate>
+@interface YRNBeaconDetailViewController ()
 
 @property (weak, nonatomic) IBOutlet UILabel *regionLabel;
 @property (weak, nonatomic) IBOutlet UILabel *beaconLabel;
@@ -19,76 +17,21 @@
 @property (weak, nonatomic) IBOutlet UILabel *majorLabel;
 @property (weak, nonatomic) IBOutlet UILabel *minorLabel;
 
-@property (nonatomic, strong) YRNBeaconManager *beaconManager;
-
 @end
-
 
 @implementation YRNBeaconDetailViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self)
-    {
-    }
-    return self;
-}
-
-- (id)initWithCoder:(NSCoder *)aDecoder
-{
-    self = [super initWithCoder:aDecoder];
-    
-    if (self)
-    {
-        [self setBeaconManager:[[YRNBeaconManager alloc] initWithConfiguration:@"BeaconRegions"]];
-        [[self beaconManager] setDelegate:self];
-    }
-    
-    return self;
-}
+#pragma mark - UIViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
-    [[self beaconManager] registerBeaconRegionsFromConfigurationFile];
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-#pragma mark - YRNBeaconManagerDelegate methods
-
-- (void)beaconManager:(YRNBeaconManager *)manager didEnterRegion:(CLBeaconRegion *)region
-{
-    [[self regionLabel] setText:[region identifier]];
-    [[self statusLabel] setText:@"Entered a region"];
-}
-
-- (void)beaconManager:(YRNBeaconManager *)manager didExitRegion:(CLBeaconRegion *)region
-{
-    [[self regionLabel] setText:@""];
-    [[self beaconLabel] setText:@""];
-    [[self distanceLabel] setText:@""];
-    [[self majorLabel] setText:@""];
-    [[self minorLabel] setText:@""];
-    [[self statusLabel] setText:@"Exited a region"];
-}
-
-- (void)beaconManager:(YRNBeaconManager *)manager didRangeBeacons:(NSArray *)beacons inRegion:(CLBeaconRegion *)region
-{
     NSArray *proximityArray = @[@"Unknown", @"Immediate", @"Near", @"Far"];
-    CLBeacon *nearestBeacon = [beacons firstObject];
     
-    [[self regionLabel] setText:[region identifier]];
-    [[self beaconLabel] setText:[[nearestBeacon proximityUUID] UUIDString]];
-    [[self majorLabel] setText:[[nearestBeacon major] stringValue]];
-    [[self minorLabel] setText:[[nearestBeacon minor] stringValue]];
-    [[self distanceLabel] setText:[proximityArray objectAtIndex:[nearestBeacon proximity]]];
+    [[self beaconLabel] setText:[[[self beacon] proximityUUID] UUIDString]];
+    [[self majorLabel] setText:[[[self beacon] major] stringValue]];
+    [[self minorLabel] setText:[[[self beacon] minor] stringValue]];
+    [[self distanceLabel] setText:[proximityArray objectAtIndex:[[self beacon] proximity]]];
     [[self statusLabel] setText:@"Ranged a beacon"];
 }
 
