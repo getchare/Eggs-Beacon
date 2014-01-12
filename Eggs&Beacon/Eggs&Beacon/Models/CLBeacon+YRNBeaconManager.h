001846eb47bde8f832674cb2c0696a9aa929afa9
Mouhcine <mouhcine.elamine@icloud.com>
2014-01-12 20:08:15 +0100

    Added CLBeacon+YRNBeaconManager

---
+++ Eggs&Beacon/Eggs&Beacon/Models/CLBeacon+YRNBeaconManager.h
@@ -0,0 +1,13 @@
+//
+//  CLBeacon+YRNBeaconManager.h
+//  Eggs&Beacon
+//
+//  Created by Mouhcine El Amine on 12/01/14.
+//  Copyright (c) 2014 Yron Lab. All rights reserved.
+//
+
+#import <CoreLocation/CoreLocation.h>
+
+@interface CLBeacon (YRNBeaconManager)
+
+@end
---
+++ Eggs&Beacon/Eggs&Beacon/Models/CLBeacon+YRNBeaconManager.m
@@ -0,0 +1,13 @@
+//
+//  CLBeacon+YRNBeaconManager.m
+//  Eggs&Beacon
+//
+//  Created by Mouhcine El Amine on 12/01/14.
+//  Copyright (c) 2014 Yron Lab. All rights reserved.
+//
+
+#import "CLBeacon+YRNBeaconManager.h"
+
+@implementation CLBeacon (YRNBeaconManager)
+
+@end
