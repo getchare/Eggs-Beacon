1c41d074f04e78c1dbbb32d9d251ef34e15cfe09
Mouhcine <mouhcine.elamine@icloud.com>
2014-01-12 20:04:40 +0100

    Added UIColor+YRNBeacon

---
+++ Eggs&Beacon/Eggs&Beacon/Models/UIColor+YRNBeacon.m
@@ -0,0 +1,13 @@
+//
+//  UIColor+YRNBeacon.m
+//  Eggs&Beacon
+//
+//  Created by Mouhcine El Amine on 12/01/14.
+//  Copyright (c) 2014 Yron Lab. All rights reserved.
+//
+
+#import "UIColor+YRNBeacon.h"
+
+@implementation UIColor (YRNBeacon)
+
+@end
