01a03e97d0f2b296be9b129d018997eac4da88a5
Mouhcine <mouhcine.elamine@icloud.com>
2014-01-12 16:53:29 +0100

    Added YRNBeaconProximityView

---
+++ Eggs&Beacon/Eggs&Beacon/Views/YRNBeaconProximityView.h
@@ -0,0 +1,13 @@
+//
+//  YRNBeaconProximityView.h
+//  Eggs&Beacon
+//
+//  Created by Mouhcine El Amine on 12/01/14.
+//  Copyright (c) 2014 Yron Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YRNBeaconProximityView : UIView
+
+@end
---
+++ Eggs&Beacon/Eggs&Beacon/Views/YRNBeaconProximityView.m
@@ -0,0 +1,31 @@
+//
+//  YRNBeaconProximityView.m
+//  Eggs&Beacon
+//
+//  Created by Mouhcine El Amine on 12/01/14.
+//  Copyright (c) 2014 Yron Lab. All rights reserved.
+//
+
+#import "YRNBeaconProximityView.h"
+
+@implementation YRNBeaconProximityView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
