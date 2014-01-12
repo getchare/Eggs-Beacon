7f51d905fb3ad9108f0431870afef4981a7fa6c9
Mouhcine <mouhcine.elamine@icloud.com>
2014-01-12 16:45:36 +0100

    Added YRNBeaconCell

---
+++ Eggs&Beacon/Eggs&Beacon/Views/YRNBeaconCell.h
@@ -0,0 +1,13 @@
+//
+//  YRNBeaconCell.h
+//  Eggs&Beacon
+//
+//  Created by Mouhcine El Amine on 12/01/14.
+//  Copyright (c) 2014 Yron Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YRNBeaconCell : UITableViewCell
+
+@end
---
+++ Eggs&Beacon/Eggs&Beacon/Views/YRNBeaconCell.m
@@ -0,0 +1,29 @@
+//
+//  YRNBeaconCell.m
+//  Eggs&Beacon
+//
+//  Created by Mouhcine El Amine on 12/01/14.
+//  Copyright (c) 2014 Yron Lab. All rights reserved.
+//
+
+#import "YRNBeaconCell.h"
+
+@implementation YRNBeaconCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
