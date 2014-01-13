27ff20d3e80f8eb566f4d22c5bcf99fd46cc5c90
Mouhcine <mouhcine.elamine@icloud.com>
2014-01-13 01:04:01 +0100

    Added YRNBluetoothHUD

---
+++ Eggs&Beacon/Eggs&Beacon/Views/YRNBluetoothHUD.h
@@ -0,0 +1,13 @@
+//
+//  YRNBluetoothHUD.h
+//  Eggs&Beacon
+//
+//  Created by Mouhcine El Amine on 13/01/14.
+//  Copyright (c) 2014 Yron Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YRNBluetoothHUD : UIView
+
+@end
---
+++ Eggs&Beacon/Eggs&Beacon/Views/YRNBluetoothHUD.m
@@ -0,0 +1,31 @@
+//
+//  YRNBluetoothHUD.m
+//  Eggs&Beacon
+//
+//  Created by Mouhcine El Amine on 13/01/14.
+//  Copyright (c) 2014 Yron Lab. All rights reserved.
+//
+
+#import "YRNBluetoothHUD.h"
+
+@implementation YRNBluetoothHUD
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
